f58dc2be0600c14e5bc640ee8d3b714f46581201
mrstruijk <mrstruijk@gmail.com>
2024-09-16 18:05:08 +0200

    Init

---
+++ Runtime/TagSelectorAttribute.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+
+namespace SOSXR.TagSelector
+{
+    /// <summary>
+    ///     From: https://www.brechtos.com/tagselectorattribute/
+    ///     Usage:
+    ///     public string TagFilter = "";
+    ///     public string[] TagFilterArray = new string[] { };
+    /// </summary>
+    public class TagSelectorAttribute : PropertyAttribute
+    {
+        public bool UseDefaultTagFieldDrawer = false;
+    }
+}
